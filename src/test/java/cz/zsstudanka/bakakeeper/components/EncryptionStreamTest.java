92d7784e0750ac0759d1caf105d88f4264bd1c5e
jhlade <jan.hladena@gmail.com>
2020-08-28 12:01:58 +0200

    zjednodušení základního testu

--- src/test/java/cz/zsstudanka/bakakeeper/components/EncryptionStreamTest.java
+++ src/test/java/cz/zsstudanka/bakakeeper/components/EncryptionStreamTest.java
@@ -5,29 +5,21 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
+/**
+ * Testy pro proudové zpracování AES/GCM šifrovaných dat.
+ *
+ * @author Jan Hladěna
+ */
 public class EncryptionStreamTest {
 
     @Test
-    public void basicEncryptionTest() {
+    public void basicEncryptionTest() throws Exception {
 
         String password = "t4jnE.he$l@";
         String message = "Příliš žluťoučký kůň úpěl ďábelské ódy v Bakalářích.";
 
-        String encryptedMessage = "";
-
-        try {
-            encryptedMessage = EncryptionManager.encrypt(message, password);
-        } catch (Exception e) {
-            //
-        }
-
-        String decryptedMessage = "";
-
-        try {
-            decryptedMessage = EncryptionManager.decrypt(encryptedMessage, password);
-        } catch (Exception e) {
-            //
-        }
+        String encryptedMessage = EncryptionManager.encrypt(message, password);
+        String decryptedMessage = EncryptionManager.decrypt(encryptedMessage, password);
 
         assertEquals(message, decryptedMessage);
     }
