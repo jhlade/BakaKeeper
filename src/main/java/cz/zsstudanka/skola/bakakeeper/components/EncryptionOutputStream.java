576c411c8dc014f321833f0e1f76b88123bd01dc
jhlade <jan.hladena@gmail.com>
2020-09-04 16:14:45 +0200

    refaktorizace šifrovaného výstupního proudu

--- src/main/java/cz/zsstudanka/skola/bakakeeper/components/EncryptionOutputStream.java
+++ src/main/java/cz/zsstudanka/skola/bakakeeper/components/EncryptionOutputStream.java
@@ -11,14 +11,23 @@ import java.io.OutputStream;
  */
 public class EncryptionOutputStream extends FilterOutputStream {
 
+    /** velikost vnitřního bufferu */
+    private final static int BUFFER_SIZE = 4096;
+
+    /** interní buffer */
+    private byte[] buffer;
+
+    /** ukazatel do interního bufferu */
+    private int bIndex;
+
     /** heslo */
     private final String PASSPHRASE;
 
-    /** vstupní data o maximální velikosti jednoho paketu */
-    private byte[] BUFFER = new byte[ EncryptionManager.CHUNK_SIZE ];
+    /** jeden paket */
+    private byte[] CHUNK;
 
-    /** aktuální počet vstupních bajtů */
-    private int count;
+    /** aktuální ukazatel do paketu */
+    private int cIndex;
 
     /**
      * Výchozí konstruktor.
@@ -28,11 +37,18 @@ public class EncryptionOutputStream extends FilterOutputStream {
      */
     public EncryptionOutputStream(OutputStream out, String passphrase) {
         super(out);
+
+        this.buffer = null;
+        this.bIndex = 0;
+
         this.PASSPHRASE = passphrase;
+
+        this.CHUNK = new byte[ EncryptionManager.CHUNK_SIZE ];
+        this.cIndex = 0;
     }
 
     /**
-     * Aleternativní konstruktor s prázdným heslem.
+     * Alternativní konstruktor s prázdným heslem.
      *
      * @param out nižší vrstva
      */
@@ -41,107 +57,131 @@ public class EncryptionOutputStream extends FilterOutputStream {
     }
 
     /**
-     * Zápis jednoho bajtu do bufferu. Pokud je překročena velikost bufferu (maximální velikost paketu),
-     * je buffer vyprázdněn a data zašifrována.
+     * Zápis jednoho bajtu.
      *
-     * @param b vstupní bajt
+     * @param b požadovaný bajt
      * @throws IOException
      */
     @Override
     public void write(int b) throws IOException {
 
-        if (count == BUFFER.length) {
-            flush();
+        if (buffer == null) {
+            buffer = new byte[ BUFFER_SIZE ];
         }
 
-        BUFFER[count] = (byte)(b & 0xff);
-        ++count;
+        // naplnění interního bufferu
+        if (bIndex >= buffer.length) {
+            internalWrite(buffer, 0, bIndex);
+            bIndex = 0;
+        }
+
+        buffer[bIndex++] = (byte) b;
     }
 
     /**
      * Zápis pole bajtů.
      *
-     * @param b pole bajtů
-     * @throws IOException
-     */
-    @Override
-    public void write(byte[] b) throws IOException {
-        // zápis po jednom bajtu
-        for (int i = 0; i < b.length; i++) {
-            write(b[i]);
-        }
-    }
-
-    /**
-     * Základní zápis dat.
-     *
-     * @param b pole bajtů
+     * @param b bajtové pole
      * @param off offset
-     * @param len délka dat
+     * @param len celková délka
      * @throws IOException
      */
     @Override
     public void write(byte[] b, int off, int len) throws IOException {
-
-        // pokud je volno, překopíruje se obsah do bufferu
-        if (len < (this.BUFFER.length - count) ) {
-            System.arraycopy(b, off, this.BUFFER, count, len);
-            count += len;
-        } else {
-            // pokud už není volno, provede se vyprázdnění bufferu
-            flush();
+        if (len <= 0) {
+            return;
         }
+
+        flushBuffer();
+        internalWrite(b, off, len);
     }
 
     /**
-     * Okamžité vyprázdnění bufferu a provedení zápisu zašifrovného paketu do další vrstvy.
+     * Zápis celého pole bajtů.
+     *
+     * @param b pole batjů
+     * @throws IOException
+     */
+    @Override
+    public void write(byte[] b) throws IOException {
+        write(b, 0, b.length);
+    }
+
+    /**
+     * Uzavření výstupu.
      *
      * @throws IOException
      */
     @Override
-    public void flush() throws IOException {
+    public void close() throws IOException {
 
-        // žádná data
-        if (count == 0) {
-            return;
+        if (cIndex != 0) {
+            writeChunk(cIndex);
         }
 
-        // příprava dat k zašifrování
-        byte[] dataToEncrypt = new byte[ count ];
-        // pokud čítač označuje celý paket, současný buffer se okamžitě použije
-        if (count == EncryptionManager.CHUNK_SIZE) {
-            dataToEncrypt = BUFFER;
-        } else {
-            // zkopírování zbytku
-            System.arraycopy(BUFFER, 0, dataToEncrypt, 0, count);
+        out.close();
+    }
+
+    /**
+     * Interní provedení zašifrovaného zápisu po dosažení hranice paketu.
+     *
+     * @param b data k zašifrování
+     * @param off offset
+     * @param len celková délka
+     * @throws IOException
+     */
+    private void internalWrite(byte[] b, int off, int len) throws IOException {
+
+        int fill;
+        for (fill = off; fill < len; fill++) {
+
+            CHUNK[cIndex] = b[fill];
+            cIndex++;
+
+            // dosažena hranice paketu
+            if (cIndex == EncryptionManager.CHUNK_SIZE) {
+                writeChunk(cIndex);
+            }
         }
+    }
+
+    /**
+     * Zápis zašifrovaného paketu na podřízený výstup.
+     *
+     * @param size požadovaná délka dat
+     * @throws IOException
+     */
+    private void writeChunk(int size) throws IOException {
+
+        // přípava dat k zašifrování
+        byte[] dataToEncrypt = new byte[size];
+        System.arraycopy(CHUNK, 0, dataToEncrypt, 0, size);
 
         try {
             // provedení zašifrování paketu a odeslání výsledných dat do nižší vrstvy
             out.write(EncryptionManager.encrypt(dataToEncrypt, PASSPHRASE), 0,
-                    count // počet bajtů čistých dat
+                    size // počet bajtů čistých dat
                             + EncryptionManager.IV_BYTES // velikost inicializačního vektoru
                             + EncryptionManager.SALT_BYTES // délka soli
                             + EncryptionManager.TAG_BITS/8); // autentizační značka
 
         } catch (Exception e) {
             throw new IOException(e.getLocalizedMessage());
+        } finally {
+            // resetování ukazatele do interního paketu
+            cIndex = 0;
         }
-
-        // nastavení počítadla zpátky na začátek bufferu
-        count = 0;
-        // vyprázdnění nižší vrstvy
-        out.flush();
     }
 
     /**
-     * Ukončení výstupních operací.
+     * Vyprázdní načtený interní buffer.
      *
      * @throws IOException
      */
-    @Override
-    public void close() throws IOException {
-        flush();
-        out.close();
+    private void flushBuffer() throws IOException {
+        if (bIndex > 0) {
+            internalWrite(buffer, 0, bIndex);
+            bIndex = 0;
+        }
     }
 }
