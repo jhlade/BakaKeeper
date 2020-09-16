b74dfcdb39c2c26e226d1cad0b3302ae0ee881d6
jhlade <jan.hladena@gmail.com>
2020-09-16 13:06:42 +0200

    poznámka k UAC

--- src/main/java/cz/zsstudanka/skola/bakakeeper/constants/EBakaUAC.java
+++ src/main/java/cz/zsstudanka/skola/bakakeeper/constants/EBakaUAC.java
@@ -7,8 +7,12 @@ package cz.zsstudanka.skola.bakakeeper.constants;
  */
 public enum EBakaUAC {
 
+    /**
+     * Zde jsou vypsány všechny vlastnosti objektu uživatele podporované současným MS AD,
+     * pro účel zpracování účtů žáků a případně personálu budou potřeba pouze některé.
+     */
     SCRIPT (0x0001),
-    ACCOUNTDISABLE (0x0002), // účet uzamčen
+    ACCOUNTDISABLE (0x0002), // účet je uzamčen
     HOMEDIR_REQUIRED (0x0008),
     LOCKOUT (0x0010),
     PASSWD_NOTREQD (0x0020),
@@ -19,14 +23,14 @@ public enum EBakaUAC {
     INTERDOMAIN_TRUST_ACCOUNT (0x0800),
     WORKSTATION_TRUST_ACCOUNT (0x1000),
     SERVER_TRUST_ACCOUNT (0x2000),
-    DONT_EXPIRE_PASSWORD (0x10000), // heslo nikdy nevyprší
+    DONT_EXPIRE_PASSWORD (0x10000), // platnost hesla nikdy nevyprší
     MNS_LOGON_ACCOUNT (0x20000),
     SMARTCARD_REQUIRED (0x40000),
     TRUSTED_FOR_DELEGATION (0x80000),
     NOT_DELEGATED (0x100000),
     USE_DES_KEY_ONLY (0x200000),
     DONT_REQ_PREAUTH (0x400000),
-    PASSWORD_EXPIRED (0x800000), // platnost hesla vypršela
+    PASSWORD_EXPIRED (0x800000), // platnost hesla již vypršela
     TRUSTED_TO_AUTH_FOR_DELEGATION (0x1000000),
     PARTIAL_SECRETS_ACCOUNT (0x04000000);
 
